3cad3a342efabffd62dad4a4d41f314d466ebe10
Shivam Pande <shivshivampande18@gmail.com>
2024-07-29 10:48:14 +0530

    Create looping.asm

---
+++ looping.asm
@@ -0,0 +1,31 @@
+	AREA SHIVAM, DATA, READONLY
+	EXPORT __Vectors
+
+__Vectors
+	DCD 0x10001000;
+	DCD Reset_Handler
+	ALIGN
+	AREA mycode, CODE, READONLY
+	ENTRY
+	EXPORT Reset_Handler
+Reset_Handler
+	LDR R0, =N
+	LDR R1, [R0]
+	LDR R2, =0XA1243140
+	LDR R0, =N
+	LDR R1, =DST
+	MOV R3, #4
+BACK LDR R2, [R0], #4; #-4 for negative indexing
+	STR R2, [R1], #4
+	SUB R3, #1
+	CMP R3, #0
+	BNE BACK 
+	
+STOP 
+	B STOP
+	
+N DCD 0x12345678,0x12345671,0x12345333,0x1230000
+
+	AREA data, DATA, READWRITE
+DST DCD 0
+	END
